4b7ba94dffa9b5baad044d01eff6d7f19e253e46
steven-skyconsultancy <steven@skyconsultancymns.com>
2025-06-29 19:51:11 +0800

    vercel package json

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    //appDir: true, // ✅ 告诉 Next.js 我用了 app router 结构
-  },
+  }
 };
 
 export default nextConfig;
